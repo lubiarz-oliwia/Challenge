1041f3bff48b0c0f307de30c5a6dc468b6e5ec0a
oliwia lubiarz <lubiarzoliwia@gmail.com>
2021-04-12 23:39:16 +0200

    working API

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import Card from 'react-bootstrap/Card';
+import { getDetails } from "../API/constants";
+
 
 const style1 = {
 textAlign: "center"
 };
 
-export const Details = ({userId, id, title, body}) => (
-  
+export const Details = ({userId, id, title, body}) => {
 
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+      getDetails(id, setDetails);
+  }, []);
+
+
+
+return (
     <div>
       <h1 style={style1}>Details</h1>
       {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-      <h4>UserId: {userId}</h4>
-      <h4>Id: {id}</h4>
-      <h4>Title: {title}</h4>
-      <h4>Body: {body}</h4>
+      <h4>UserId: {details.userId}</h4>
+      <h4>Id: {details.id}</h4>
+      <h4>Title: {details.title}</h4>
+      <h4>Body: {details.body}</h4>
   </div>
-);
+)
+};
 
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tile } from '../Tile/Tile';
 import { ButtonComponent } from '../Button/Button';
 import { Searchbox } from '../Searchbox/Searchbox';
+import { getPosts } from "../API/constants";
 
 
-export const MainPage = () => {
+export const MainPage = ({setPostId}) => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPosts(setPosts);
+    }, []);
+
+
     return (
-        <div>
+        <>
             <ButtonComponent variant='dark' children="Log out"></ButtonComponent>
-            <Searchbox/>
-            <Tile />
-        </div>
+            <Searchbox />
+            <div>
+                {posts.map((item, index) => {
+                    return (
+                        <Tile
+                            userId={item.userId}
+                            title={item.title}
+                            postId={item.id}
+                            onTileClick={setPostId}
+                        />
+                    )
+                })
+                }
+            </div>
+        </>
     )
 }
